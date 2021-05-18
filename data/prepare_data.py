1560945189a6ededed1f159c93baf16c0ec0f2fa
MattGarton <matthew.t.garton@gmail.com>
2021-05-18 19:41:09 -0400

    updating repo

---
+++ data/prepare_data.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+prepare_data.py
+
+Data preparation script - takes in raw data and converts to a time series
+format with prices and features that a trading algo or ML model can ingest
+
+Author: Matthew Garton
+"""
---
+++ src/utils/data_ingestion.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+data_ingestion.py
+
+Helper functions for assembling dataset to be used in algorithms
+
+Author: Matthew Garton
+"""
