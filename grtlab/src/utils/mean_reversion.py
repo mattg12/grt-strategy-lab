7ebb573e4ae2c469cf9676320a3c94a6088a0aa2
MattGarton <matthew.t.garton@gmail.com>
2021-05-25 10:51:31 -0400

    started mean reversion work

---
+++ grtlab/src/utils/mean_reversion.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+mean_reversion.py
+
+Module of helper functions for mean reversion algorithms
+Note that it will be more useful in most cases to implement open source
+implementations of such tests
+
+@author: Matthew Garton
+"""
+import numpy as np
+
+def generate_hurst_exponent(ts):
+    """
+    Generate the Hurst exponent of a time-series. Formulated based on
+    the method described in QuantStart article titled 'Basics of Statistical
+    Mean Reversion Testing' found here
+    https://www.quantstart.com/articles/Basics-of-Statistical-Mean-Reversion-Testing/
+
+    Rule of thumb:
+        if H < 0.5: the series exhibits stationarity; mean reversion strategies
+            may work
+        if H = 0.5: the series is a random walk; unlikely to find a strategy 
+            based on prices alone
+        if H > 0.5; the series has a trend; momentum strategies may work
+
+    Parameters
+    ----------
+    ts : numpy array-like
+        array of time series values
+
+    Returns
+    -------
+    h: float
+        Estimate of hurst exponent for time series
+    """
+    
+    #  TODO: test or understand different ranges to check
+    # initiate range of lagged values - smaller n may be more accurate
+    lags = range(2, 20)
+
+    # calculate the array of variances of lagged differences
+    tau = [np.sqrt(np.std(np.subtract(ts[lag:], ts[:-lag]))) for lag in lags]
+
+    # estimate the Hurst exponent
+    poly = np.polyfit(np.log(lags), np.log(tau), 1)
+    
+    # return the Hurst exponent
+    return poly[0]*2
